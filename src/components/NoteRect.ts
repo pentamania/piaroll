2534bbc35b5dfafdde647c98d6aef12cef49eea4
pentamania <tanigami7220@gmail.com>
2019-06-21 23:07:41 +0900

    [change/clean] use abstractSvg class for BrushRect, clean NoteRect

--- src/components/NoteRect.ts
+++ src/components/NoteRect.ts
@@ -1,86 +1,90 @@
 import {SVG_NAMESPACE} from "../config";
 import EventEmitter from 'wolfy87-eventemitter';
 const DEFAULT_FILL = "#FE7A8E";
-const HITBOX_WIDTH = 4;
+const STROKE_WIDTH = 2;
+const ACTIVE_STROKE_STYLE = "#FFF";
+const EXTENSION_RECT_WIDTH = 4;
+const EXTENSTION_RECT_FILL = "#ce3939";
 
 /**
- * class NoteRect
+ * @class NoteRect
  */
 export class NoteRect extends EventEmitter {
 
+  private _x = 0
+  private _y = 0
   private _width = 0
   private _height = 0
   private _tick = 0
   private _trackId = 0
-  private _activeStrokeStyle = "#FFF"
+  private _activeStrokeStyle = ACTIVE_STROKE_STYLE
   private _fillStyle
   private _selected = false
-  private _image
-  private _index: number
   private _id: number
   duration: number
-  svgElement: SVGSVGElement
-  rectElement: SVGRectElement
-  hitBoxElement: SVGRectElement
   tempStartX: number
   tempStartY: number
 
+  private containerElement: SVGSVGElement
+  rectElement: SVGRectElement
+  hitBoxElement: SVGRectElement
+  private _debugTextElement
+
   constructor(color: string = DEFAULT_FILL) {
     super();
-    // TODO: グループ化するため、svgにする
-    this.svgElement = document.createElementNS(SVG_NAMESPACE, "svg");
 
+    // container svg
+    this.containerElement = document.createElementNS(SVG_NAMESPACE, "svg");
+
+    // main rect svg
     this.rectElement = document.createElementNS(SVG_NAMESPACE, "rect");
     // this.svgElement = document.createElementNS(SVG_NAMESPACE, "rect");
-    this.rectElement.setAttribute('stroke-width', "2");
+    this.rectElement.setAttribute('stroke-width', String(STROKE_WIDTH));
     this.fill = color;
-    this.svgElement.appendChild(this.rectElement);
+    this.containerElement.appendChild(this.rectElement);
 
-    // TODO: 伸ばし判定用の端矩形
+    // extension rect on edge
     this.hitBoxElement = document.createElementNS(SVG_NAMESPACE, "rect");
-    this.hitBoxElement.setAttribute('width', String(HITBOX_WIDTH));
-    this.hitBoxElement.setAttribute('fill', "#ff0000");
-    this.svgElement.appendChild(this.hitBoxElement);
+    this.hitBoxElement.setAttribute('width', String(EXTENSION_RECT_WIDTH));
+    this.hitBoxElement.setAttribute('fill', EXTENSTION_RECT_FILL);
+    this.containerElement.appendChild(this.hitBoxElement);
 
     this._debugTextElement = document.createElementNS(SVG_NAMESPACE, "text")
     this._debugTextElement.setAttribute('y', "16");
-    this.svgElement.appendChild(this._debugTextElement);
+    this.containerElement.appendChild(this._debugTextElement);
+
     // TODO: 画像を追加できるようにする
   }
-
 
   get id() { return this._id; }
   set id(v) {
     this._id = v;
     // this._debugTextElement.textContent = v;
   }
-  // get index() { return this._index; }
-  // set index(v) {
-  //   this._index = v;
-  //   this._debugTextElement.textContent = v;
-  // }
-
+  // get x() { return Number(this.svgElement.getAttribute('x')); }
+  get x() { return this._x; }
+  set x(v: number) {
+    this._x = v;
+    this.containerElement.setAttribute('x', String(v));
+    // this.rectElement.setAttribute('x', v);
+  }
+  get y() { return this._y; }
+  set y(v: number) {
+    this._y = v;
+    this.containerElement.setAttribute('y', String(v));
+    // this.rectElement.setAttribute('y', v);
+  }
   get width() { return this._width; }
   set width(v) {
     this._width = v;
     this.rectElement.setAttribute("width", String(v));
-    this.hitBoxElement.setAttribute("x", String(v - HITBOX_WIDTH));
+    this.hitBoxElement.setAttribute("x", String(v - EXTENSION_RECT_WIDTH));
   }
   get height() { return this._height; }
   set height(v) {
     this._height = v;
     this.rectElement.setAttribute("height", String(v));
     this.hitBoxElement.setAttribute('height', String(v));
-  }
-  get x() { return Number(this.svgElement.getAttribute('x'));}
-  set x(v) {
-    this.svgElement.setAttribute('x', v);
-    // this.rectElement.setAttribute('x', v);
-  }
-  get y() { return Number(this.svgElement.getAttribute('y')); }
-  set y(v) {
-    this.svgElement.setAttribute('y', v);
-    // this.rectElement.setAttribute('y', v);
   }
   get selected() { return this._selected; }
   set selected(v) {
@@ -106,12 +110,11 @@ export class NoteRect extends EventEmitter {
   }
 
   append(parent) {
-    parent.appendChild(this.svgElement);
-    // console.log(this.svgElement.parentElement);
+    parent.appendChild(this.containerElement);
   }
 
   remove() {
-    this.svgElement.parentElement.removeChild(this.svgElement);
+    this.containerElement.parentElement.removeChild(this.containerElement);
     this.emit('removed');
   }
 
